59ba08242fba135b39071981a20bbb28e8e439fc
AleGnucci <alegnu1@gmail.com>
2019-07-17 12:00:57 +0200

    Added optimized version of the job

--- src/main/scala/it/unibo/agnucci/spark/job/OptimizedTagRankingJob.scala
+++ src/main/scala/it/unibo/agnucci/spark/job/OptimizedTagRankingJob.scala
@@ -4,7 +4,7 @@ import it.unibo.agnucci.spark.job.helpers.HelperMethods._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{Row, SparkSession}
 
-object TagRankingJob {
+object OptimizedTagRankingJob {
 
   private val spark = SparkSession
     .builder()
@@ -37,42 +37,40 @@ object TagRankingJob {
   /**
     * Calculates for each tag the average trending time and the videos count.
     * */
-  private def getAverageTrendingTimeAndVideosCount(rddVideosNoError: RDD[Row]): RDD[Row] = {
-    //removing useless fields and calculating trending time for each row
-    //fields in this rdd: tags, trendingTime
-    val rddVideosWithTrendingTime = rddVideosNoError //TODO: qui converti a rdd di coppie k-v
-      .map(row => Row.fromSeq(keepOnlyTagsField(row.toSeq) :+
-        getTrendingTimeDays(row.getAs[String](1), row.getAs[String](0))))
+  private def getAverageTrendingTimeAndVideosCount(rddVideosNoError: RDD[Row]): RDD[(String, (Long, Long))] = {
+    //removes useless fields and calculates trending time for each row
+    //also it makes every double quotation mark a single quotation mark
+    //pair rdd: the key is tags, the value is trendingTime
+    val rddVideosWithTrendingTime = rddVideosNoError
+      .map(row => (correctTags(row.getAs[String](2)), getTrendingTimeDays(row.getAs[String](1), row.getAs[String](0))))
 
-    //makes every double quotation mark a single quotation mark
-    //fields in this rdd: tags, trendingTime
-    val rddVideosWithCorrectTags = rddVideosWithTrendingTime
-      .map(row => Row.fromSeq(Seq(correctTags(row.getAs[String](0)), row.get(1))))
+    //creating for each pair as many new pairs as the amount of tags for that initial pair
+    //pair rdd: the key is the tag, the value is trendingTime and videosCount (this last one always has value 1)
+    val rddTags = rddVideosWithTrendingTime.flatMap(pair => pair._1.split("\\|").map(tag => (tag, (pair._2, 1L))))
 
-    //creating for each row as many new rows as the amount of tags for that initial row
-    //fields in this rdd: tag, trendingTime, videosCount (this last one always has value 1)
-    val rddTags = rddVideosWithCorrectTags
-      .flatMap(row => row.getAs[String](0).split("\\|").map(tag => createRowWithSingleTag(row, tag)))
-
-    /*grouping the rows by tag, then aggregating the groups to calculate for each tag the videos count and the sum
+    /*aggregates the groups to calculate for each tag the videos count and the sum
     of trending times (to be later used to calculate the mean trending time)*/
-    val rddTagsWithTrendingTimeSum = rddTags.groupBy(row => row.get(0)).map(rowGroup => createAggregatedRow(rowGroup)) //TODO: qui usa aggregateByKey
+    //pair rdd: the key is the tag, the value is trendingTime and videosCount
+    val rddTagsWithTrendingTimeSum = rddTags
+      .reduceByKey((value1, value2) => (value1._1 + value2._1, value1._2 + value2._2))
 
     //calculating the mean trending time
-    //fields in this rdd: tag, meanTrendingTime, videosCount
-    rddTagsWithTrendingTimeSum
-      .map(row => Row.fromSeq(Seq(row.get(0), row.getAs[Long](1) / row.getAs[Long](2), row.get(2))))
+    //pair rdd: the key is the tag, the value is meanTrendingTime and videosCount
+    rddTagsWithTrendingTimeSum.mapValues(value => (value._1 / value._2, value._2))
   }
 
   /**
     * Sorts the rdd by videos count, then saves the result in a parquet file.
     * */
-  private def sortAndSaveToParquet(rddTagsWithTrendingTimeAverage: RDD[Row], args: Array[String]): Unit = {
+  private def sortAndSaveToParquet(notSortedRdd: RDD[(String, (Long, Long))], args: Array[String]): Unit = {
     //sorting the results by videos count
-    val sortedRdd = rddTagsWithTrendingTimeAverage.sortBy(_.getAs[Long](2), ascending = false)
+    val sortedRdd = notSortedRdd.sortBy(_._2._2, ascending = false)
+
+    //converted the pair rdd to a row rdd
+    val rowRdd = sortedRdd.map(pair => Row.fromTuple(pair._1, pair._2._1, pair._2._2))
 
     //puts output in a single partition
-    val resultRdd = sortedRdd coalesce 1
+    val resultRdd = rowRdd coalesce 1
 
     //deletes the output folder if it already exists
     deletePathIfExists(args(1), sc)
@@ -80,4 +78,5 @@ object TagRankingJob {
     //saving the result in a parquet file
     spark.createDataFrame(resultRdd, getOutputParquetSchema).write.parquet(args(1))
   }
+
 }
